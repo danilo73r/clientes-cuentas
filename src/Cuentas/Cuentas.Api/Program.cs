0bd106f404a5917f8eedfcb014b1cd341544aa69
Danilo A. <32546402+danilo73r@users.noreply.github.com>
2026-09-18 07:04:22 -0500

    [ADD] configurar wolverine con rabbitmq

--- src/Cuentas/Cuentas.Api/Program.cs
+++ src/Cuentas/Cuentas.Api/Program.cs
@@ -1,16 +1,28 @@
+using Wolverine;
+using Wolverine.RabbitMQ;
 using Cuentas.Infrastructure;
 using Shared.Api.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Exceptions
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
-var connectionString = builder
-    .Configuration
-    .GetConnectionString("Database")
-    ?? throw new InvalidOperationException("Falta configurar ConnectionString");
+// Database
+var connectionString = builder.Configuration.GetConnectionString("Database")
+    ?? throw new InvalidOperationException("Falta configurar ConnectionStrings:Database");
 builder.Services.AddInfrastructure(connectionString);
+
+// RabbitMQ
+var rabbitConnection = builder.Configuration.GetConnectionString("RabbitMQ")
+    ?? throw new InvalidOperationException("Falta configurar ConnectionStrings:RabbitMQ");
+
+builder.Host.UseWolverine(options =>
+{
+    options.UseRabbitMq(new Uri(rabbitConnection)).AutoProvision();
+    options.ListenToRabbitQueue("cuentas");
+});
 
 var app = builder.Build();
 
