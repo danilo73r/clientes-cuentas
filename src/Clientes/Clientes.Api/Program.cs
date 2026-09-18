30f5a55939bfa194b9dfd2e2c9e1d58378b848db
Danilo A. <32546402+danilo73r@users.noreply.github.com>
2026-09-18 05:26:56 -0500

    [FIX] UseExceptionHandler necesita AddProblemDetails

--- src/Clientes/Clientes.Api/Program.cs
+++ src/Clientes/Clientes.Api/Program.cs
@@ -1,8 +1,8 @@
 using Shared.Api.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 var app = builder.Build();
 
 // middlewares
