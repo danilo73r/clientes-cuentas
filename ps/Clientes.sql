26ce457e0d5f183f28275dad82e0bc262e50c122
Danilo A. <32546402+danilo73r@users.noreply.github.com>
2026-09-18 19:53:43 -0500

    [ADD] generacion de scripql para clientes a partir de migraciones

---
+++ ps/Clientes.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+CREATE TABLE "Personas" (
+    "Id" uuid NOT NULL,
+    "Nombre" text NOT NULL,
+    "Genero" text NOT NULL,
+    "FechaNacimiento" date NOT NULL,
+    "Identificacion" text NOT NULL,
+    "Direccion" text NOT NULL,
+    "Telefono" text NOT NULL,
+    CONSTRAINT "PK_Personas" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE "Clientes" (
+    "Id" uuid NOT NULL,
+    "Contrasena" text NOT NULL,
+    "Estado" boolean NOT NULL,
+    "Version" bigint NOT NULL,
+    "OperacionPendienteId" uuid,
+    CONSTRAINT "PK_Clientes" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_Clientes_Personas_Id" FOREIGN KEY ("Id") REFERENCES "Personas" ("Id") ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX "UX_Personas_Identificacion" ON "Personas" ("Identificacion");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20260919004612_Inicial_20260918194606914', '10.0.4');
+
+COMMIT;
+
---
+++ src/Clientes/Clientes.Infrastructure/Persistencia/Migrations/20260919004612_Inicial_20260918194606914.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clientes.Infrastructure.Persistencia.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial_20260918194606914 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Genero = table.Column<string>(type: "text", nullable: false),
+                    FechaNacimiento = table.Column<DateOnly>(type: "date", nullable: false),
+                    Identificacion = table.Column<string>(type: "text", nullable: false),
+                    Direccion = table.Column<string>(type: "text", nullable: false),
+                    Telefono = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Contrasena = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    Version = table.Column<long>(type: "bigint", nullable: false),
+                    OperacionPendienteId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Personas_Id",
+                        column: x => x.Id,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UX_Personas_Identificacion",
+                table: "Personas",
+                column: "Identificacion",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
